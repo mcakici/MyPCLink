6698507bcc7c119ce24f264ee4cfb55687df8cfa
Mustafa Çakıcı <glikoz@live.com>
2021-03-31 11:38:58 +0300

    just modified one more

--- pc_calculator/tests/test_views.py
+++ pc_calculator/tests/test_views.py
@@ -3,14 +3,15 @@ from django.test import Client
 from django.urls import reverse
 from pc_calculator.models import *
 
-#reverse('pc-calc:upload')
+# reverse('pc-calc:upload')
+
 
 class UploadViewLoggedInTest(TestCase):
 
     def test_logged_out_user_access(self):
         response = self.client.get(reverse('pc-calc:upload'))
-        self.assertRedirects(response, '/accounts/login/?next=/upload/', status_code=302)
-
+        self.assertRedirects(
+            response, '/accounts/login/?next=/upload/', status_code=302)
 
     def test_logged_in_user_access(self):
         User.objects.create_user('testuser', 'testuser@gmail.com', '123456a.')
